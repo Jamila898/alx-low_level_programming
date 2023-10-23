3a8ecad1875010673d8fa1b501f389d0dbb51f8b
Jamila <husjamilaahmed89@gmail.com>
2023-10-23 15:03:42 +0100

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n) {
     unsigned int i;
 
     for (i = 0; i < n; i++) {
-        s[i] = b;  // Set the current byte to the constant value
+        s[i] = b;
     }
 
     return s;
